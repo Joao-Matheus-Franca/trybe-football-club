682bbecb699b396ab88a0b12b87ea29cb981187f
João Matheus Silva Franca <matheussfranca@live.com>
2023-03-12 18:09:34 -0300

    Concluído: Requisito 13

---
+++ app/backend/src/database/models/matchesModel.ts
@@ -0,0 +1,54 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './teamsModel';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeamId: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamId: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    underscored: true,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+
+Teams.hasMany(Matches);
+Matches.belongsTo(Teams);
+
+export default Matches;
